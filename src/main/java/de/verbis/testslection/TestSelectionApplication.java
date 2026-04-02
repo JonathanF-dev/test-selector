762f66a60cc8bed4892dedf50c59b6006256a681
Jonathan <jonathan.freund@yahoo.com>
2026-04-02 08:49:22 +0200

    Einstiegsklasse festgelegt

--- src/main/java/de/verbis/testslection/TestSelectionApplication.java
+++ src/main/java/de/verbis/testslection/TestSelectionApplication.java
@@ -1,3 +1,8 @@
+package de.verbis.testslection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class TestSelectionApplication {
     public static void main(String[] args) {
